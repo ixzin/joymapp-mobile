e87e3e23e9fd5f808d616390bd983fe5bdcfbc49
alex <alexey@example.com>
2017-03-30 10:45:50 +0300

    Write new route to server

--- app/routeCreation.js
+++ app/routeCreation.js
@@ -5,19 +5,21 @@ import {
   Text,
   TouchableHighlight,
   Image,
+  Picker,
   TextInput,
   ScrollView,
   View
 } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import DatePicker from 'react-native-datepicker';
-import MapView from 'react-native-maps';
+import Icon from './icon'; 
 import  mainStyles from './styles';
 class routeCreationScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
       name:'',
+      type:'',
       description:'',
       startDate:'',
       endDate:''
@@ -26,12 +28,35 @@ class routeCreationScreen extends Component {
    async goToRoute() {
       let token=await AsyncStorage.getItem('token');
       let user=await AsyncStorage.getItem('user');
-      let userId=user._id;
+      let userId=JSON.parse(user)._id;
       this.createNewRoute(userId,token).then(function(response) {
         console.log(response);
        });
     }
     createNewRoute(id,token) {
+       return fetch('http://teethemes.com:3000/api/routes/',{
+          method: 'POST',
+          headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+              'x-access-token':token
+        },
+        body: JSON.stringify({
+          type: this.state.type,
+          name: this.state.name,
+          owner:id,
+          status:'planned',
+          description: this.state.description,
+          startDate: this.state.startDate,
+          endDate: this.state.endDate
+        })
+        }).then((response) => response.json())
+            .then((responseJSON) => {
+                return Promise.resolve(responseJSON);  
+            })
+            .catch((error) => {
+              return Promise.reject(error);
+            });
     }
     render() {
     return (
@@ -41,6 +66,20 @@ class routeCreationScreen extends Component {
         <ScrollView style={{zIndex:3}}>
           <View style={styles.routeForm}>
             <Text style={mainStyles.header}>New route</Text>
+            <View style={{height:60,borderBottomWidth:2,borderBottomColor:'white'}}>
+              <Picker
+                selectedValue={this.state.type}
+                style={{height:60,minWidth:200,marginTop:0,marginBottom:10,color:'white'}}
+                onValueChange={(type) => this.setState({type: type})}>
+                <Picker.Item label="Choose type" value=""/>
+                <Picker.Item label="hike" value="hike"/>
+                <Picker.Item label="marine" value="marine" />
+                <Picker.Item label="auto" value="auto" />
+                <Picker.Item label="city" value="city" />
+                <Picker.Item label="extreme" value="extreme" />
+                <Picker.Item label="event" value="event" />
+              </Picker>
+            </View>
             <TextInput
               style={mainStyles.input}
               placeholder="Title"
@@ -59,7 +98,7 @@ class routeCreationScreen extends Component {
                           style={styles.dateInput}
                           date={this.state.startDate}
                           mode="date"
-                          placeholder='start'
+                          placeholder='start date'
                           format="YYYY-MM-DD"
                           minDate="1950-01-01"
                           maxDate="2000-01-01"
@@ -81,7 +120,7 @@ class routeCreationScreen extends Component {
                           style={styles.dateInput}
                           date={this.state.endtDate}
                           mode="date"
-                          placeholder='start'
+                          placeholder='end date'
                           format="YYYY-MM-DD"
                           minDate="1950-01-01"
                           maxDate="2000-01-01"
@@ -98,16 +137,10 @@ class routeCreationScreen extends Component {
                             }
                           }}
                           onDateChange={(endtDate) => {this.setState({endtDate: endtDate})}}
-                        />   
-                   <TextInput
-                  style={mainStyles.input}
-                  placeholder="start point"
-                  placeholderTextColor="white"
-                  onChangeText={(startPoint) => this.setState({startPoint})}
-                />     
+                        />     
                <TouchableHighlight onPress={()=>this.goToRoute()} style={mainStyles.Button}>
-                            <Text style={{color:'white',textAlign:'center'}}>Go!</Text>
-                </TouchableHighlight>       
+                    <Text style={{color:'white',textAlign:'center'}}>Go!</Text>
+              </TouchableHighlight>       
             </View>
           </ScrollView>
       </View>
@@ -121,7 +154,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     zIndex:2,
-    paddingTop:60
+    paddingTop:60,
   },
   dateInput:{
     height:40,
